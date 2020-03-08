5f22c42b426bb8be5695d7e76c8734d638021baa
pcpractic <61136016+pcpractic@users.noreply.github.com>
2020-03-08 09:44:10 +0100

    Update php-fpm-scale-off.sh

--- php-fpm-scale-off.sh
+++ php-fpm-scale-off.sh
@@ -1,1 +1,2 @@
 sed -i s/php-fpm.openebs.svc.cluster.local:9000/127.0.0.1:9000/g /etc/nginx/nginx.conf && service nginx stop && service nginx reload && service nginx start &
+echo "PHP-FPM switched to localhost successfully"
